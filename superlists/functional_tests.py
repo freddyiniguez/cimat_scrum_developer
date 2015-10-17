8d9569a9b0efee82edebc812d989b3ef809be7cd
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-10-17 14:51:25 -0500

    Chapter 2: script modified and autopep8 applied.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-	self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -18,7 +18,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        print("testing!!!")
         self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
