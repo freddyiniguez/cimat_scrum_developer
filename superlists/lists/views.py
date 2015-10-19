3a30f01e4262c388153563ea1014e55ea4d3b0a8
pauEscarcia <paulina_151090@hotmail.com>
2015-10-19 00:20:05 -0500

    Chapter 5: Saving the POST to the Database

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def home_page(request):
+	if request.method == 'POST':
+		new_item_text = request.POST['item_text'] 
+		Item.objects.create(text=new_item_text) 
+	else:
+		new_item_text = '' 
+		
 	return render(request, 'home.html', {
-	'new_item_text': request.POST['item_text', ''],
+		'new_item_text': new_item_text, 
 	})
